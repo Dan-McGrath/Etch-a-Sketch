1d05d22b753fd73af14911f8d3b8913204f9b8e8
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-20 13:57:14 -0400

    Add default colors

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const container = document.querySelector('.container');
+const colorPicker = document.getElementById('pick-color')
+const defaultColors = [
+    'black',
+    'grey',
+    'blue',
+    'darkGreen',
+    'fireBrick',
+    'gold'
+]
+
+//allow user to click and drag colors
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 
+const addColor = (e, color = 'black') => {
+    if(e.type === 'mouseover' && !mouseDown) {
+        return
+    }
+    e.target.dataset.active = 'true';
+    e.target.classList.add(color) 
+}
+
+const removeColor = (e, color) => {
+    e.target.classList.remove(color);
+}
 
 const createGrid = (num = 16) => {
-    
     for (let i = 0; i < num; i++) {
         const row = document.createElement('div');
         row.classList.add('rows');
@@ -13,29 +37,29 @@ const createGrid = (num = 16) => {
             const col = document.createElement('div');
             col.classList.add('cols');
             row.appendChild(col);
+            col.addEventListener('mousedown', addColor);
+            col.addEventListener('mouseover', addColor)
         }
-    }
-    
+    } 
 }
+
 
 createGrid();
 
 const cell = document.querySelectorAll('.cols');
-const cellArray = Array.from(cell)
 
-const changeCell = (e, color = 'black') => {
-    if (e.target.dataset.active === 'true') {
-        e.target.dataset.active = 'false';
-        e.target.classList.remove(color)
-    } else {
-        e.target.dataset.active = 'true';
-        e.target.classList.add(color)
-    }
 
+
+
+const getColor = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        const option = document.createElement('option');
+        option.classList.add(arr[i]);
+        colorPicker.appendChild(option)
+
+    } 
 }
 
 
-for (let i = 0; i < cellArray.length; i++) {
-    cellArray[i].addEventListener('click', changeCell)
-}
 
+getColor(defaultColors);
